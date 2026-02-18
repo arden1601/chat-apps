031fed98ea3a8dd2c8f4a80ca7e3f55ff875e91c
arden1601 <corneliusardensatwikahermawan@mail.ugm.ac.id>
2026-02-18 20:02:03 +0700

    feat: create migration for avatar and user table

--- database/migrations/2026_02_18_125104_add_last_seen_and_avatar_to_user_table.php
+++ database/migrations/2026_02_18_125104_add_last_seen_and_avatar_to_user_table.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table("users", function (Blueprint $table) {
+            $table->timestamp("last_seen")->nullable();
+            $table->string("avatar")->nullable();
             //
         });
     }
@@ -21,7 +22,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table("users", function (Blueprint $table) {
+            $table->dropColumn("last_seen");
+            $table->dropColumn("avatar");
             //
         });
     }
